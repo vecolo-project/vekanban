ed460349932039318942dd1667ad93e3d06c69a6
Nouu <noelarrieulacoste@yahoo.fr>
2021-03-12 18:11:27 +0100

    add check combo box with controller

--- src/main/java/fr/vecolo/vekanban/controller/UiController.java
+++ src/main/java/fr/vecolo/vekanban/controller/UiController.java
@@ -1,9 +1,13 @@
-package fr.vecolo.vekanban;
+package fr.vecolo.vekanban.controller;
 
+import fr.vecolo.vekanban.config.StageListener;
 import javafx.application.HostServices;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.util.StringConverter;
+import org.controlsfx.control.CheckComboBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -19,6 +23,9 @@ public class UiController {
     @FXML
     public Button button;
 
+    @FXML
+    private CheckComboBox<CheckBox> checkComboBox;
+
     @Autowired
     public UiController(HostServices hostServices, StageListener stageListener) {
         this.hostServices = hostServices;
@@ -28,11 +35,31 @@ public class UiController {
     @FXML
     public void initialize() {
         this.button.setOnAction(e -> buttonCLicked());
+        checkComboBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(CheckBox object) {
+                return object.getText();
+            }
+
+            @Override
+            public CheckBox fromString(String string) {
+                return new CheckBox(string);
+            }
+        });
+        fillCheckComboBox();
     }
 
     private void buttonCLicked() {
         this.label.setText(hostServices.getDocumentBase());
+        checkComboBox.getCheckModel().getCheckedItems().forEach(System.out::println);
         stageListener.ifThisWorks();
     }
 
+    private void fillCheckComboBox() {
+        checkComboBox.getItems().clear();
+        for (int i = 0; i < 10; i++) {
+            checkComboBox.getItems()
+                    .add(new CheckBox("Button " + (i + 1)));
+        }
+    }
 }
