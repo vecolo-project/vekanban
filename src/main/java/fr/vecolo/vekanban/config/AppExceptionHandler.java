54a91ac9b1e4d0d18337db31fd2908e84f6b03d0
Nouu <noelarrieulacoste@yahoo.fr>
2021-03-27 23:01:26 +0100

    [VCL-11] exception handler with logger

---
+++ src/main/java/fr/vecolo/vekanban/config/AppExceptionHandler.java
@@ -0,0 +1,21 @@
+package fr.vecolo.vekanban.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(AppExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public void GlobalExceptionHandler(Exception ex) {
+        logger.error(ex.getMessage());
+        String stackTrace = Arrays.stream(ex.getStackTrace()).limit(5).map(StackTraceElement::toString).collect(Collectors.joining("\n"));
+        logger.error(stackTrace);
+    }
+}
---
+++ src/main/java/fr/vecolo/vekanban/config/exceptions/UserRessourceException.java
@@ -0,0 +1,7 @@
+package fr.vecolo.vekanban.config.exceptions;
+
+public class UserRessourceException extends Exception {
+    public UserRessourceException(String message) {
+        super(message);
+    }
+}
