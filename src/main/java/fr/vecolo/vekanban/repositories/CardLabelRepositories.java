a13912bcfdb0b6b1dbc191ac67c603eaec87eb79
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-04-01 16:19:45 +0200

    card label repository

---
+++ src/main/java/fr/vecolo/vekanban/repositories/CardLabelRepositories.java
@@ -0,0 +1,16 @@
+package fr.vecolo.vekanban.repositories;
+
+import fr.vecolo.vekanban.models.Board;
+import fr.vecolo.vekanban.models.Card;
+import fr.vecolo.vekanban.models.CardStatus;
+import fr.vecolo.vekanban.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CardLabelRepositories extends JpaRepository<Card, Long> {
+    //TODO create service with toto,in_progress,done
+    List<Card> findByAssignedBoardAndStatus(Board assignedBoard, CardStatus status);
+
+    List<Card> findByAssignedUserAndAssignedBoard(User assignedUser, Board assignedBoard);
+}
