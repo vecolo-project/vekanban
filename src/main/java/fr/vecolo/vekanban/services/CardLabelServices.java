758d9ddf07c922a7254a662435b672c9e18d6fe3
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-27 16:58:05 +0200

    begin impl card label services

--- src/main/java/fr/vecolo/vekanban/services/CardLabelServices.java
+++ src/main/java/fr/vecolo/vekanban/services/CardLabelServices.java
@@ -1,4 +1,17 @@
 package fr.vecolo.vekanban.services;
 
-public class CardLabelServices {
+import fr.vecolo.vekanban.models.Board;
+import fr.vecolo.vekanban.models.CardLabel;
+
+import java.util.List;
+
+public interface CardLabelServices {
+
+    CardLabel getCardLabelById(Long id);
+
+    List<CardLabel> getAllCardLabelFromBoard(Board board);
+
+    CardLabel saveOrUdateCardLabel(CardLabel cardLabel);
+
+    void deleteCardLabel(CardLabel cardLabel);
 }
---
+++ src/main/java/fr/vecolo/vekanban/services/CardLabelServicesImpl.java
@@ -0,0 +1,24 @@
+package fr.vecolo.vekanban.services;
+
+import fr.vecolo.vekanban.models.Board;
+import fr.vecolo.vekanban.models.CardLabel;
+
+import java.util.List;
+
+public class CardLabelServicesImpl {
+
+    CardLabel getCardLabelById(Long id) {
+        return null;
+    }
+
+    List<CardLabel> getAllCardLabelFromBoard(Board board) {
+        return null;
+    }
+
+    CardLabel saveOrUdateCardLabel(CardLabel cardLabel) {
+        return null;
+    }
+
+    void deleteCardLabel(CardLabel cardLabel) {
+    }
+}
