77dc5edfd0a15789fd628d2e21f8e514b6255992
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-27 16:57:52 +0200

    refactor bugs between card and card labels

--- src/main/java/fr/vecolo/vekanban/repositories/CardLabelRepositories.java
+++ src/main/java/fr/vecolo/vekanban/repositories/CardLabelRepositories.java
@@ -1,16 +1,11 @@
 package fr.vecolo.vekanban.repositories;
 
 import fr.vecolo.vekanban.models.Board;
-import fr.vecolo.vekanban.models.Card;
-import fr.vecolo.vekanban.models.CardStatus;
-import fr.vecolo.vekanban.models.User;
+import fr.vecolo.vekanban.models.CardLabel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CardLabelRepositories extends JpaRepository<Card, Long> {
-
-    List<Card> findByAssignedBoardAndStatus(Board assignedBoard, CardStatus status);
-
-    List<Card> findByAssignedUserAndAssignedBoard(User assignedUser, Board assignedBoard);
+public interface CardLabelRepositories extends JpaRepository<CardLabel, Long> {
+    List<CardLabel> findAllByBoard(Board board);
 }
--- src/main/java/fr/vecolo/vekanban/repositories/CardRepository.java
+++ src/main/java/fr/vecolo/vekanban/repositories/CardRepository.java
@@ -1,7 +1,17 @@
 package fr.vecolo.vekanban.repositories;
 
+import fr.vecolo.vekanban.models.Board;
 import fr.vecolo.vekanban.models.Card;
+import fr.vecolo.vekanban.models.CardStatus;
+import fr.vecolo.vekanban.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    List<Card> findByAssignedBoardAndStatus(Board assignedBoard, CardStatus status);
+
+    List<Card> findByAssignedUserAndAssignedBoard(User assignedUser, Board assignedBoard);
+
 }
